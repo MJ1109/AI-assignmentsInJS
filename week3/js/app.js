c9d855c43725f5ea9d72b9a82ec06b136171f702
MJ1109 <manuja115@gmail.com>
2021-03-07 13:37:01 +0100

    cat/dog excersize from the class added

--- week3/js/app.js
+++ week3/js/app.js
@@ -8,3 +8,12 @@ knn.learn([12, 2, 3], "cat")
 knn.learn([11, 1.7, 2.5], "cat")
 
 //learn the dog
+knn.learn([22, 5, 8], "dog")
+knn.learn([26, 7, 9], "dog")
+knn.learn([24, 9, 9.7], "dog")
+
+console.log("learning completed!")
+
+//hij heeft nu geleerd, dus predicten kan starten
+let unknownAnimal = [12,8, 10]
+console.log(`I think this is a ${knn.classify(unknownAnimal)}`)
